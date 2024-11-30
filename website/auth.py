3cfaa8c96baccca285992ac130cb92644c51c44b
eKipruto <edmond.kipruto@gmail.com>
2024-11-30 09:08:01 +0300

    Added database, forms:signupform, loginform, and chagepasswordform alongside their respective backend codings

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,17 @@
-from flask import Blueprint
-from flask_login import current_user
+from flask import Blueprint, render_template, flash, redirect
+from .forms import LoginForm, SignUpForm #PasswordChangeForm
+from .models import Customer
+from . import db
+from flask_login import login_user, login_required, logout_user
 
 auth=Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return 'This is the login page'
+    form=LoginForm()
+    return render_template('login.html', form=form)
 
 @auth.route('/sign_up')
 def sign_up():
-    return 'This is the sign_up page'
+    form=SignUpForm()
+    return render_template('signup.html', form=form)
---
+++ website/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, EmailField, FloatField, StringField, PasswordField,BooleanField,SubmitField
+from wtforms.validators import length,DataRequired,NumberRange
+
+class SignUpForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), length(min=2)])
+    password1 = PasswordField('Enter Your Password', validators=[DataRequired(), length(min=6)])
+    password2 = PasswordField('Confirm Your Password', validators=[DataRequired(), length(min=6)])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email=EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Enter Your Password', validators=[DataRequired()])
+    submit = SubmitField('Login In')
